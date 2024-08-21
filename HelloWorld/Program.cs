e800d9402f4fa954a6141e0e6caf9dd9e428557f
DanteMinecraft <dantewallen07@gmail.com>
2024-08-21 14:59:46 +0200

    dishwasher

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+//Console.BackgroundColor = ConsoleColor.Cyan;
+//Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("Morsning tågkorsning!");
+Console.Write("Vad heter du? ");
+String namn = Console.ReadLine();
+Console.WriteLine("Hej " + namn + "!");
+
+Console.Write("Vad gillar du att göra på fritiden? ");
+String intresse = Console.ReadLine();
+Console.WriteLine("Vad kul att höra! Jag tycker också om " + intresse + "!");
+
+Console.Write("Vad är din favorit OS-gren? ");
+String os = Console.ReadLine();
+Console.WriteLine("Jag håller helt med om att " + os + "är en bra gren!");

adb635dcd9b9a57079dac05f9f8b6f12f0c27101
Dante Wallén <dantewallen07@gmail.com>
2024-08-26 14:44:54 +0200

    if-statement

--- Kapitel-1/Registrering/Program.cs
+++ Kapitel-1/Registrering/Program.cs
@@ -11,4 +11,10 @@ Console.Write("Ange efternamn: ");
 string lastName = Console.ReadLine();
 
 Console.WriteLine("Hej " + firstName + " " + lastName + "!");
-Console.WriteLine($"Hej (förnamn) (efternamn)!");
+Console.WriteLine($"Hej {firstName} {lastName}!");
+
+// Är det Dante Wallén som loggar in
+if (firstName == "Dante" && lastName == "Wallén")
+{
+    Console.WriteLine("Nej men titta vem som loggade in!");
+}

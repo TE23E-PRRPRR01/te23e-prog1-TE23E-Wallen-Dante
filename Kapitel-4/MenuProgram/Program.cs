18343bd16ee03f75c3fa897ee588f4334d5521a4
Dante Wallén <dante.wallen@elev.ntig.se>
2024-09-23 14:48:55 +0200

    Tränat på while-loopar samt filhantering

---
+++ Kapitel-4/MenuProgram/Program.cs
@@ -0,0 +1,76 @@
+﻿// Ett form av program för att visa en meny i konsollen
+
+Console.Clear();
+Console.WriteLine("Välkommen till Unika Maträtter! Här under ser du våran meny av alla otänkbara måltider!");
+
+// Variabel för valda produkter
+string choices = "";
+
+// Variabel för antalet beställda maträtter
+int amount = 0;
+
+// Aktiv loop tills användaren valt att avsluta
+while (true)
+{
+
+    Console.WriteLine("""
+    Välj ett av följande alternativ genom att ange siffran före produkten du vill köpa, exempelevis "1".
+    
+    Meny | Unika Maträtter
+    1. Betongpulverpaj
+    2. Daggmasksmet
+    3. Hästskosoppa
+    4. Tepåsar i tomatsås
+
+    5. Gå till kassan.
+    6. Avsluta beställningen.
+
+    """);
+    Console.Write("Ditt svar: ");
+
+    choices = Console.ReadLine();
+
+    // Hantera användarens val
+
+    if (choices == "1")
+    {
+        Console.WriteLine($"Du har lagt i betongpulverpaj i kundvagnen. Vad mer önskar du att beställa?");
+        amount++;
+    }
+    else if (choices == "2")
+    {
+        Console.WriteLine($"Du har lagt i daggmasksmet i kundvagnen. Vad mer önskar du att beställa?");
+        amount++;
+    }
+    else if (choices == "3")
+    {
+        Console.WriteLine($"Du har lagt i hästskosoppa i kundvagnen. Vad mer önskar du att beställa?");
+        amount++;
+    }
+    else if (choices == "4")
+    {
+        Console.WriteLine($"Du har lagt i tepåsar i tomatsås i kundvagnen. Vad mer önskar du att beställa?");
+        amount++;
+    }
+    else if (choices == "5")
+    {
+
+        Console.WriteLine($"""
+        Du har beställt {amount} varor. Detta kommer att kosta betydligt mer än vad du har råd med.
+        Vi skickar dig tillbaka till menyn, men vi har tagit bort lite alternativ, för att avsluta
+        då vi vet att du inte har råd med detta för att ge dig en chans att avsluta innan det är för sent.
+        """);
+    }
+    else if (choices == "6")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("""
+        Vi på Unika Maträtter förstår inte det du angav som svar.
+        Kom ihåg att svara mellan 1-6.
+        """);
+    }
+    
+}
---
+++ Kapitel-4/Texteditor/Program.cs
@@ -0,0 +1,53 @@
+﻿// Program för att exportera text till textfil samt för att läsa från en textfil
+
+Console.Clear();
+
+while (true)
+{
+
+    Console.WriteLine("""
+    Välkommen till det här fantastiska programmet som tillåter dig att exportera och läsa av textfiler.
+
+    Vad vill du börja med?
+    ----------------------
+    1. Exportera skriven text till en textfil
+    2. Importera och läs en rext från en textfil
+    3. Avsluta programmet
+    """);
+
+    string programChoice = Console.ReadLine();
+
+    string text = "";
+    string filename = "";
+    if (programChoice == "1")
+    {
+        Console.WriteLine("Du har valt att exportera en text till en textfil. Skriv texten du vill exportera nedan.");
+        text = Console.ReadLine();
+        File.WriteAllText("export.txt", text);
+
+    }
+    else if (programChoice == "2")
+    {
+        Console.WriteLine("Du har valt att läsa av en text från en textfil. Skriv in filnamnet på filen du vill läsa av nedan.");
+        filename = Console.ReadLine();
+
+        Console.WriteLine("""
+        ----------------------------------------------
+
+        """);
+        File.ReadAllText($"{filename}.txt");
+        Console.WriteLine("""
+
+        ----------------------------------------------
+        """);
+    }
+    else if (programChoice == "3")
+    {
+        Console.WriteLine("Programmet avslutas.");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Du angav ett felaktigt svar. Försök igen.");
+    }
+}

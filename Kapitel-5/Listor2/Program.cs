33418126d50a113c390512323b4e63ea5f178d4d
Dante Wallén <dante.wallen@elev.ntig.se>
2024-12-16 14:48:06 +0100

    försök till miniprogram om filmer

---
+++ Kapitel-5/Listor2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Skapa en tom lista
+
+List<string> listaNamn = [];
+
+Console.Write("Vilket namn vill du lägga till? vara här: ");
+
+// Infoga i en lista
+listaNamn.Add(Console.ReadLine());
+
+// Skriv ut listan
+Console.WriteLine("Här är listan efter att du lade till ett namn:");
+
+// Namn i listan
+foreach (var namn in listaNamn)
+{
+    Console.WriteLine($"● {namn}");
+}
+
+while (true)
+{
+
+    Console.WriteLine("Vill du skriva in ett till namn i listan? y/n");
+    if (Console.ReadLine() == "y")
+    {
+
+        Console.WriteLine("Vilket namn vill du lägga till? vara här: ");
+        listaNamn.Add(Console.ReadLine());
+
+        Console.WriteLine("Här är listan efter att du lade till ett namn:");
+        foreach (var namn in listaNamn) { Console.WriteLine($"● {namn}"); }
+
+    } else {
+        Console.WriteLine("Programmet avslutas.");
+        break;
+    }
+}
---
+++ Mikroövningar/Lista - Mikroövningar 3/Program.cs
@@ -0,0 +1,67 @@
+﻿Console.Clear();
+
+// Tomma listor för kategorierna action, fantasy, och sci-fi
+List<string> listaFilmerAction = [];
+
+// Intro
+Console.WriteLine("Välkommen!");
+
+// Programloop
+while (true)
+{
+
+    // Meny till programmet där användaren kan välja resultat
+    Console.WriteLine("""
+    -----Programmeny-----
+
+    1. Lägg till en actionfilm
+    2. Ta bort en actionfilm
+    4. Visa alla filmer ur en kategori
+
+    5. Avsluta programmet
+    ---------------------
+    """);
+
+    // Kontrollerar användarens inmatning
+    if (Console.ReadLine() == "1")
+    {
+        // Frågar användaren om vilken film hen vill lägga till
+        Console.WriteLine("Vad heter filmen du vill lägga till?");
+
+        // Sparar filmen i listan
+        listaFilmerAction.Add(Console.ReadLine());
+
+        // Meddelar användaren om att filmen läggs till och om den uppdaterade listan
+        Console.WriteLine($"""
+        Filmen {Console.ReadLine()} har lagts till i listan.
+
+        Listan ser nu ut så här:
+        """);
+
+        // Visar den uppdaterade listar
+        foreach (var film in listaFilmerAction)
+        {
+            Console.WriteLine($"x {film}");
+        }
+    }
+    else if (Console.ReadLine() == "2")
+    {
+
+    }
+    else if (Console.ReadLine() == "3")
+    {
+
+    }
+    else if (Console.ReadLine() == "4")
+    {
+
+    }
+    else if (Console.ReadLine() == "5")
+    {
+
+    }
+    else
+    {
+        Console.WriteLine("Din inmatning var felaktig. Försök igen.");
+    }
+}

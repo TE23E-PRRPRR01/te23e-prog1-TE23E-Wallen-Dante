8337decfe5af32a1c60b35f061d3e0503deeb305
Dante Wallén <dante.wallen@elev.ntig.se>
2025-01-08 15:03:26 +0100

    Massor av metoder - kvadrerande tal samt namn med string och int i metod

---
+++ Kapitel-6/EnMassaMetoder/Program.cs
@@ -0,0 +1,33 @@
+﻿/****************************************************************************
+*                                 M A I N                                   *
+****************************************************************************/
+
+Console.Clear();
+
+// Call method with "Cal" as string value
+SayHelloName("Cal");
+
+// Call method with "5" as int value
+ShowNumberSquared(5);
+
+/****************************************************************************
+*                              M E T O D E R                                *
+****************************************************************************/
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="name">Name</param>
+static void SayHelloName(string name) {
+    Console.WriteLine($"Hello {name}!");
+
+}
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="number">Number</param>
+static void ShowNumberSquared(int number) {
+    Console.WriteLine($"{number} squared is {number * number}");
+
+}

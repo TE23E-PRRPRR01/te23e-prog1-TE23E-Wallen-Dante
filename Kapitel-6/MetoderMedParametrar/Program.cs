0baeff3a720fc963f1dde404ccd4a79756642210
Dante Wallén <dante.wallen@elev.ntig.se>
2025-01-13 14:20:14 +0100

    Vertikalt meddelande, samt optimerat vertikalt meddelande, + kvadrat med hjälp av metoder :)

---
+++ Kapitel-6/MetoderMedParametrar/Program.cs
@@ -0,0 +1,64 @@
+﻿/****************************************************************************
+*                                 M A I N                                   *
+****************************************************************************/
+Console.Clear();
+
+Console.WriteLine("Program med metoder");
+
+//********************************
+// SKRIV UT MEDDELANDE VERTIKALT *
+//********************************
+
+//Om vi inte hade haft metoder hade vi skrivit såhär:
+
+string meddelande = "Måndag"; // Variabel för meddelande som ska loopas
+
+for (var i = 0; i < meddelande.Length; i++) // Loopar lika många gånger som bokstäver i "Måndag"
+{
+    Console.WriteLine(meddelande[i]); // Skriver ut det i konsolen.
+}
+
+// I och med att vi har metoder, så kan man även skriva såhär:
+
+SkrivVertikalt("Tisdag"); // Kallar på metoden 'SkrivVertikalt' med innehållet "Tisdag" för att definiera vad som ska skrivas ut
+
+//***************************************************
+// SKRIV UT FYRKANT MED SYMBOLER - utan kommentarer *
+//***************************************************
+
+Console.Clear(); // Kommentera ut den här om du vill se resultatet av den ovanstående koden
+
+RitaFyrkant(3);
+
+/****************************************************************************
+*                              M E T O D E R                                *
+****************************************************************************/
+
+/// <summary>
+/// Metod för att skriva ut vertikalt
+/// </summary>
+/// <param name="meddelande1">Vertikalt meddelande</param>
+static void SkrivVertikalt(string meddelande1) // En metod med namnet 'SkrivVertikalt' som skapar en ny string inuti sig, 'meddelande1'
+{
+    // En till likadan for-loop som tidigare, fast med metodens string istället för 'meddelande' (gamla)
+    for (var i = 0; i < meddelande1.Length; i++)
+    {
+        Console.WriteLine(meddelande1[i]); // Skriver ut 'meddelande1' (nya) på samma sätt som 'meddelande' (gamla)
+    }
+}
+
+/// <summary>
+/// Metod för att skriva ut en kvadrat
+/// </summary>
+/// <param name="sidlängd">Längd på sida</param>
+static void RitaFyrkant(int sidlängd)
+{
+    for (var i = 0; i < sidlängd; i++)
+    {
+        for (var j = 0; j < sidlängd; j++)
+        {
+            Console.Write("■ ");
+        }
+        Console.WriteLine();
+    }
+}
